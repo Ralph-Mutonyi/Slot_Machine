1aad881e6853b6770aeba6e37abf1b84adfa6f8f
RALPH MUTONYI <ralfmutonyi@gmail.com>
2024-05-28 22:58:04 +0300

    Finished step 2: Determine number of lines

--- project.js
+++ project.js
@@ -13,21 +13,38 @@
 const prompt = require("prompt-sync")();
 
 const deposit = () => {
+    //endless loop if everything is amount is invalid
     while(true){
-    const depositAmount = prompt("Enter a deposit Amount: ");
-    
-    // convert the deposit amount to a number
-    const numberDepositAmount = parseFloat(depositAmount);
+        const depositAmount = prompt("Enter a deposit Amount: ");
+        
+        // convert the deposit amount to a number
+        const numberDepositAmount = parseFloat(depositAmount);
 
-    // check if value prompted fro user  is a number 
-    if(isNaN(numberDepositAmount) || numberDepositAmount <= 0){
-        console.log("Invalid deposit amount, try again");
-    }else{
-        return numberDepositAmount;
-    }
+        // check if value prompted fro user  is a number 
+        if(isNaN(numberDepositAmount) || numberDepositAmount <= 0){
+            console.log("Invalid deposit amount, try again");
+        }else{
+            return numberDepositAmount;
+        }
     }
     
 };
 
+// step 2
+// Determine number of lines and collect bet money 
+const getNumberOfLines = () => {
+    while(true){
+        const lines = prompt("Enter the number of lines to bet on (1 - 3)");
+        const numberOfLines = parseFloat(lines);
+
+        if(isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3){
+            console.log("Invalid number of lines, Try again");
+        }else{
+            return numberOfLines;
+        }
+    }
+};
+
+
 const depositAmount = deposit();
-console.log(depositAmount);
+const numberOfLines = getNumberOfLines();
